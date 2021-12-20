f7f26fe0f9c3771e2f8a26658f89d99cb631c4b6
xiongzhentao <xiongzhentao@bsyjy.cn>
2021-12-20 09:31:33 +0800

    1

---
+++ src/Learning.Common/Cache/CacheKeyManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learning.Common.Cache
+{
+    public static class CacheKeyManager
+    {
+
+        public static string GetStudyList => "GetStudyList";
+    }
+}
---
+++ src/Learning.Common/Encrypt/Encrypt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Learning.Common.Encrypt
+{
+    public class Encrypt
+    {
+        /// <summary>
+        /// MD5 hash加密
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string MD5(string s)
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+            {
+                var t2 = BitConverter.ToString(md5.ComputeHash(Encoding.Default.GetBytes(s)), 4, 8);
+                t2 = t2.Replace("-", "");
+                t2 = t2.ToLower();
+                return t2;
+            }
+        }
+    }
+}
